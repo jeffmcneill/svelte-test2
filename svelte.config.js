5732c23126e8bbad0c227b073e08e0b34239f19d
Jeff McNeill <jeff@mcneill.io>
2022-01-30 17:42:26 +0700

    init

---
+++ svelte.config.js
@@ -0,0 +1,23 @@
+import cloudflare from '@sveltejs/adapter-cloudflare';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: cloudflare(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
+	}
+};
+
+export default config;
